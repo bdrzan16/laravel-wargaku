80c3b68245303b7d7c937fb1302d5ff30969ed23
bdrzan16 <bdrzan16@gmail.com>
2025-06-20 20:56:26 +0700

    Update AdminController untuk handle API dan Web

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,8 +10,50 @@ use Illuminate\Support\Facades\DB;
 
 class AdminController extends Controller
 {
-    public function index()
+
+    public function index(Request $request)
     {
+        // Cek apakah request dari API (misal pakai prefix atau header)
+        if ($request->wantsJson() || $request->is('api/*')) {
+            $users = User::where(function ($query) {
+                $query->whereNotNull('rw_id')->orWhereNotNull('rt_id');
+            })->get();
+
+            $chartData = array_fill(0, 12, 0);
+            foreach ($users as $user) {
+                if ($user->created_at) {
+                    $monthIndex = Carbon::parse($user->created_at)->month - 1;
+                    $chartData[$monthIndex]++;
+                }
+            }
+
+            $rwUsers = User::whereHas('rwDetail')->count();
+            $rtUsers = User::whereHas('rtDetail')->count();
+
+            $topDaerahs = Penduduk::select('daerah_id', DB::raw('count(*) as jumlah'))
+                ->groupBy('daerah_id')
+                ->orderByDesc('jumlah')
+                ->limit(5)
+                ->with('daerah')
+                ->get()
+                ->map(function ($item) {
+                    return [
+                        'nama' => $item->daerah->name,
+                        'jumlah' => $item->jumlah
+                    ];
+                });
+
+            return response()->json([
+                'pie' => [
+                    'rw' => $rwUsers,
+                    'rt' => $rtUsers,
+                ],
+                'chart' => $chartData,
+                'bar' => $topDaerahs,
+            ]);
+        }
+
+        // Kalau request dari web biasa, tampilkan Blade
         $users = User::where(function ($query) {
             $query->whereNotNull('rw_id')->orWhereNotNull('rt_id');
         })->get();
@@ -32,23 +74,52 @@ class AdminController extends Controller
             ->orderByDesc('jumlah')
             ->limit(5)
             ->with('daerah')
-            ->get()
-            ->map(function ($item) {
-                return [
-                    'nama' => $item->daerah->name,
-                    'jumlah' => $item->jumlah
-                ];
-            });
+            ->get();
 
-        return response()->json([
-            'pie' => [
-                'rw' => $rwUsers,
-                'rt' => $rtUsers,
-            ],
-            'chart' => $chartData,
-            'bar' => $topDaerahs,
-        ]);
+        $totalMax = $topDaerahs->max('jumlah');
+
+        return view('dashboard.admin', compact('chartData', 'rwUsers', 'rtUsers', 'topDaerahs', 'totalMax'));
     }
+
+    // public function index()
+    // {
+    //     $users = User::where(function ($query) {
+    //         $query->whereNotNull('rw_id')->orWhereNotNull('rt_id');
+    //     })->get();
+
+    //     $chartData = array_fill(0, 12, 0);
+    //     foreach ($users as $user) {
+    //         if ($user->created_at) {
+    //             $monthIndex = Carbon::parse($user->created_at)->month - 1;
+    //             $chartData[$monthIndex]++;
+    //         }
+    //     }
+
+    //     $rwUsers = User::whereHas('rwDetail')->count();
+    //     $rtUsers = User::whereHas('rtDetail')->count();
+
+    //     $topDaerahs = Penduduk::select('daerah_id', DB::raw('count(*) as jumlah'))
+    //         ->groupBy('daerah_id')
+    //         ->orderByDesc('jumlah')
+    //         ->limit(5)
+    //         ->with('daerah')
+    //         ->get()
+    //         ->map(function ($item) {
+    //             return [
+    //                 'nama' => $item->daerah->name,
+    //                 'jumlah' => $item->jumlah
+    //             ];
+    //         });
+
+    //     return response()->json([
+    //         'pie' => [
+    //             'rw' => $rwUsers,
+    //             'rt' => $rtUsers,
+    //         ],
+    //         'chart' => $chartData,
+    //         'bar' => $topDaerahs,
+    //     ]);
+    // }
 
     // public function index()
     // {
